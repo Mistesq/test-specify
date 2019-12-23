81f7e10b285493f02ccbf6af54fb4e6d4d0b44e2
Alexey <mistesqr@gmail.com>
2019-12-24 00:20:08 +0200

    styles mobile version

---
+++ source/js/main.js
@@ -0,0 +1,37 @@
+var mainNav = document.querySelectorAll('.page-footer__main-info');
+var head = document.querySelector('.page-header__content');
+var navToggle = document.querySelectorAll('.m-drawer__content-toggle');
+var headToggle = document.querySelector('.page-header__menu-toggle');
+var modal = document.querySelector('.modal');
+var overlay = document.querySelector('.overlay');
+var menuButton = document.querySelector('.m-drawer__content-toggle');
+
+
+headToggle.addEventListener('click', function() {
+  if (head.classList.contains('closed')) {
+    head.classList.remove('closed');
+    head.classList.add('opened');
+  }
+  else {
+    head.classList.add('closed');
+    head.classList.remove('opened');
+  }
+});
+
+const handlerMenu = () => {
+  var elem = event.target;
+  var parent = elem.parentNode;
+  
+  if (parent.classList.contains('closed')) {
+    parent.classList.remove('closed');
+    parent.classList.add('opened');
+  }
+  else {
+    parent.classList.add('closed');
+    parent.classList.remove('opened');
+  }
+};
+
+navToggle.forEach(function (item) {
+  item.addEventListener('click', handlerMenu);
+});
