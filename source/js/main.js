9999a0f8b4479b66eded23f7e85ea5e33ecd0b12
Alexey <mistesqr@gmail.com>
2019-12-24 22:02:47 +0200

    fix

--- source/js/main.js
+++ source/js/main.js
@@ -2,9 +2,6 @@ var mainNav = document.querySelectorAll('.page-footer__main-info');
 var head = document.querySelector('.page-header__content');
 var navToggle = document.querySelectorAll('.m-drawer__content-toggle');
 var headToggle = document.querySelectorAll('.page-header__menu-toggle');
-var modal = document.querySelector('.modal');
-var overlay = document.querySelector('.overlay');
-var menuButton = document.querySelector('.m-drawer__content-toggle');
 
 const headHandler = () => {
   if (head.classList.contains('closed')) {
@@ -37,4 +34,27 @@ const handlerMenu = () => {
 
 navToggle.forEach(function (item) {
   item.addEventListener('click', handlerMenu);
+});
+
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 150, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
 });
