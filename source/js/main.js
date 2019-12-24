0961b515edbb4ecb8c3f6722e6cd7793da8a151a
Alexey <mistesqr@gmail.com>
2019-12-24 18:31:51 +0200

    end main styles

--- source/js/main.js
+++ source/js/main.js
@@ -1,13 +1,12 @@
 var mainNav = document.querySelectorAll('.page-footer__main-info');
 var head = document.querySelector('.page-header__content');
 var navToggle = document.querySelectorAll('.m-drawer__content-toggle');
-var headToggle = document.querySelector('.page-header__menu-toggle');
+var headToggle = document.querySelectorAll('.page-header__menu-toggle');
 var modal = document.querySelector('.modal');
 var overlay = document.querySelector('.overlay');
 var menuButton = document.querySelector('.m-drawer__content-toggle');
 
-
-headToggle.addEventListener('click', function() {
+const headHandler = () => {
   if (head.classList.contains('closed')) {
     head.classList.remove('closed');
     head.classList.add('opened');
@@ -16,12 +15,16 @@ headToggle.addEventListener('click', function() {
     head.classList.add('closed');
     head.classList.remove('opened');
   }
+};
+
+headToggle.forEach(function (toggle) {
+  toggle.addEventListener('click', headHandler);
 });
 
 const handlerMenu = () => {
   var elem = event.target;
   var parent = elem.parentNode;
-  
+
   if (parent.classList.contains('closed')) {
     parent.classList.remove('closed');
     parent.classList.add('opened');
